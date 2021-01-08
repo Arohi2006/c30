e78aaad5b96508857eaccc4ea0c2ab0c911bd33f
Arohi2006 <mridula11@yahoo.com>
2021-01-08 19:39:23 +0530

    tint

--- Slingshot.js
+++ Slingshot.js
@@ -17,7 +17,9 @@ class SlingShot{
     fly(){
         this.sling.bodyA = null;
     }
-
+attach(bird){
+    this.sling.bodyA=bird
+}
     display(){
         image(this.sling1,200,20);
         image(this.sling2,170,20);
